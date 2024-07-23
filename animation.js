a29ea7b525c5675195f81c42c896e3d8a073be74
Riyaz1203 <mohammedriyaz12032003@gmail.com>
2024-07-23 11:05:37 +0530

    Navbar Updated

--- animation.js
+++ animation.js
@@ -1,40 +1,18 @@
-// NAVBAR ANIMATION STARTS
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggleBtn = document.getElementById('menu-toggle-btn');
-    const navContainer = document.getElementById('nav-container');
-    const navLinks = document.querySelectorAll('.nav-link');
-    const navLogo = document.querySelector('.nav-logo');
-    const navSocials = document.querySelectorAll('.nav-socials a');
-    const navFooterLinks = document.querySelectorAll('.nav-footer a');
-  
-    const tlIn = gsap.timeline({ paused: true });
-    const tlOut = gsap.timeline({ paused: true });
-  
-    // In Animation Timeline
-    tlIn.to(navContainer, { duration: 0.1, autoAlpha: 1 })
-      .from(navLogo, { y: -50, opacity: 0, duration: 0.2 })
-      .from(navLinks, { x: -50, opacity: 0, duration: 0.2, stagger: 0.1 }, '-=0.3')
-      .from(navSocials, { y: 50, opacity: 0, duration: 0.1, stagger: 0.1 }, '-=0.3')
-      .from(navFooterLinks, { opacity: 0, duration: 0.1, stagger: 0.1 }, '-=0.3');
-  
-    // Out Animation Timeline
-    tlOut.to(navFooterLinks, { opacity: 0, duration: 0.1, stagger: 0.1 })
-      .to(navSocials, { y: 50, opacity: 0, duration: 0.1, stagger: 0.1 }, '-=0.3')
-      .to(navLinks, { x: -50, opacity: 0, duration: 0.1, stagger: 0.1 }, '-=0.3')
-      .to(navLogo, { y: -50, opacity: 0, duration: 0.1 }, '-=0.3')
-      .to(navContainer, { duration: 0.1, autoAlpha: 0 });
-  
-    menuToggleBtn.addEventListener('click', () => {
-      menuToggleBtn.classList.toggle('active');
-      if (menuToggleBtn.classList.contains('active')) {
-        navContainer.classList.add('visible');
-        tlIn.restart();
-      } else {
-        tlOut.restart().then(() => {
-          navContainer.classList.remove('visible');
-        });
-      }
-    });
-  });
-  
-// NAVBAR ANIMATION END
+const open = document.querySelector('.container');
+		const close = document.querySelector('.close');
+		var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
+		open.addEventListener('click', () => {
+			if (tl.reversed()) {
+				tl.play();
+			} else {
+				tl.to('nav', { right: 0 })
+					.to('nav', { height: '100vh' }, '-=.1')
+					.to('nav ul li a', { opacity: 1, pointerEvents: 'all', stagger: .2 }, '-=.8')
+					.to('.close', { opacity: 1, pointerEvents: 'all' }, "-=.8")
+					.to('nav h2', { opacity: 1 }, '-=1');
+			}
+		});
+
+		close.addEventListener('click', () => {
+			tl.reverse();
+		});
