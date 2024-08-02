3f98de1b49cfea020397ec5acf4c6f1360230e7a
Riyaz1203 <mohammedriyaz12032003@gmail.com>
2024-08-03 02:28:01 +0530

    Pre Final Stage

--- animation.js
+++ animation.js
@@ -16,3 +16,23 @@ const open = document.querySelector('.container');
 		close.addEventListener('click', () => {
 			tl.reverse();
 		});
+
+
+
+
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.timeline({
+      scrollTrigger: {
+        trigger: ".home-title",
+        start: "top 80%",
+        end: "bottom 20%",
+        toggleActions: "play none none none",
+      }
+    })
+    .to(".home-title h1 span", {
+      duration: 1,
+      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', // Revealing the text
+      stagger: 0.2 // Delays each span animation for a staggered effect
+    });
